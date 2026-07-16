943f216820f4998095aa93d7fd245e449b17cc25
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-07-16 14:32:42 +0100

    create add book page

---
+++ app/books/create/page.tsx
@@ -0,0 +1,9 @@
+import { BookForm } from "@/components/BookForm";
+
+export default function CreateBookPage() {
+  return (
+    <main className="max-w-5xl mx-auto px-6 py-10">
+      <BookForm/>
+    </main>
+  );
+}
