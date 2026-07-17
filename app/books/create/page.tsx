f6ceae407ec641718a29ea3d7102dd737fdc8197
imaneimziline2-netizen <imaneimziline2@gmail.com>
2026-07-17 11:35:48 +0100

    implement create book page

--- app/books/create/page.tsx
+++ app/books/create/page.tsx
@@ -1,9 +1,20 @@
 import { BookForm } from "@/components/BookForm";
 
 export default function CreateBookPage() {
-  return (
-    <main className="max-w-5xl mx-auto px-6 py-10">
-      <BookForm/>
-    </main>
-  );
+    return (
+        <main className="mx-auto max-w-5xl px-6 py-12 pt-25">
+            <div className="mb-12 text-center">
+                <h1 className="font-serif text-5xl font-bold text-slate-900">
+                    Ajouter un nouveau livre
+                </h1>
+
+                <p className="mx-auto mt-4 max-w-2xl text-gray-500">
+                    Enrichissez notre collection partagée en soumettant un
+                    nouvel ouvrage à la bibliothèque Lumina.
+                </p>
+            </div>
+
+            <BookForm />
+        </main>
+    );
 }
